76d1cf88cdc0ca65977c1bcd58a2f1b657942095
barhouum7 <bb.med2000@gmail.com>
2021-01-15 21:21:41 +0100

    Basic server workflow setup

---
+++ server/index.js
@@ -0,0 +1,13 @@
+/**
+ ************* Back-End Libraries ******************
+ * bodyParser: To enable us send Post Requests.
+ * cors: To enable Cross origin requests.
+ * express: For creating the routing of our app.
+ * mongoose: To create models for the BD.
+ */
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
