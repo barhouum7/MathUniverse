df24293e3409c5bc492efaa980ec4314f1729fe2
barhouum7 <bb.med2000@gmail.com>
2021-01-15 21:19:48 +0100

    Client workflow setup

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          <h1>Our <code>~E-learning Platform</code> Welcome!</h1>
+        </p>
+        <a
+          href="https://github.com/barhouum7/MathUniverse/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Go To Main GitHub Repo.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+/**
+ ************* Front-End Libraries ******************
+ * I'm using axios for making API Requests.
+ * moment is a Library which will allows me working with time and date.
+ * I will use react-file-base64 to convert images.
+ * redux-thunk is used for asynchronous actions using redux.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  // <React.StrictMode>
+    <App />,
+  // </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
