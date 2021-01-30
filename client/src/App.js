1cbbaca215140aa0081628bd708d916fd7d818cd
barhouum7 <bb.med2000@gmail.com>
2021-01-30 09:28:39 +0100

    CLIENT: src/App.js Changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,65 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React, { useEffect } from "react"
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { connect } from "react-redux"
+import * as actions from "./store/actions"
 
-import React from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import UserLayout from "./Layouts/Layout/userLayout"
+// import ConnectionErrorHandler from "./Layouts/Error/errorHandler"
 
-import Courses from './components/Courses/Courses'
-import Form from './components/Form/Form'
-// import useStyles from './styles'
+import Dashboard from "./containers/Dashboard/Dashboard"
+import Auth from "./containers/Auth/Auth"
+import Logout from "./containers/Auth/Logout/Logout"
+import PageNotFound from "./containers/PageNotFound/PageNotFound"
+import About from "./containers/Auth/About/About"
 
-const App = () => {
-  // const classes = useStyles()
-
+const RouteUser = ({ Component, ...props }) => {
   return (
-    <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
-        Courses Control Panel
-        </Typography>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Courses />
-            </Grid>
-            <Grid item xs={12} sm={7}>
-              <Form />
-            </Grid>
-
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
-  );
+    <Route
+      {...props}
+      render={() => (
+        <UserLayout>
+          <Component />
+        </UserLayout>
+      )}
+    />
+  )
 }
 
-export default App;
+const App = ({ onTryAutoSignup }) => {
+  
+  
+
+  useEffect(() => {
+    onTryAutoSignup()
+  }, [onTryAutoSignup])
+
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
+        {/* With Layout */}
+        <RouteUser path="/" exact Component={Dashboard} />
+        <RouteUser path="/about" exact Component={About} />
+        <RouteUser path="/logout" Component={Logout} />
+        {/* Without Layout */}
+        <Route path="/sign-in" component={Auth} />
+        <Route path="/sign-up" component={Auth} />
+        {/* Page Not Found */}
+        <Route component={PageNotFound} />
+      </Switch>
+    </BrowserRouter>
+  )
+  }
+
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
