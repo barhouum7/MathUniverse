6449e6590fb7f8eb285e5f305858ee213ff31678
barhouum7 <bb.med2000@gmail.com>
2021-01-30 09:28:12 +0100

    CLIENT: src/index.js Changed

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import './index.css'
-import App from './App'
-// import reportWebVitals from './reportWebVitals'
-
 /**
  ************* Front-End Libraries ******************
  * I'm using axios for making API Requests.
@@ -11,15 +5,54 @@ import App from './App'
  * I will use react-file-base64 to convert images.
  * redux-thunk is used for asynchronous actions using redux.
  */
+import React from "react"
+import ReactDOM from "react-dom"
+import { SnackbarProvider } from "notistack"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware, compose, combineReducers } from "redux"
+import thunk from "redux-thunk"
+// import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  // <React.StrictMode>
-    <App />,
-  // </React.StrictMode>,
-  document.getElementById('root')
+import App from "./App"
+
+import ui from "./store/reducers/ui"
+import courses from "./store/reducers/courses"
+import auth from "./store/reducers/auth"
+import user from "./store/reducers/user"
+import usersManager from "./store/reducers/usersManager"
+import coursesManager from "./store/reducers/coursesManager"
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const rootReducer = combineReducers({
+  ui,
+  auth,
+  user,
+  courses,
+  usersManager,
+  coursesManager,
+})
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
 )
+
+const app = (
+  <Provider store={store}>
+    <SnackbarProvider preventDuplicate maxSnack={3}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SnackbarProvider>
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById("root"))
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// reportWebVitals()
