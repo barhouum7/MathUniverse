b7027344a57515de7378e997e3307a30f72c8880
barhouum7 <bb.med2000@gmail.com>
2021-01-30 09:32:48 +0100

    CLIENT: Added axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-const coursesURL = 'http://localhost:5000/api/courses'
-
-export const fetchCourses = () => axios.get(coursesURL)
+const instance = axios.create({
+    baseURL: 'http://localhost:5000/api',
+})
+  
+export default instance
